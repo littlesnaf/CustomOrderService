2bf6079916ae720e9614b27d15c53e2ffe9a1e6f
littlesnaf <osmanmurat03t@gmail.com>
2025-09-27 11:16:40 -0400

    abc

--- src/main/java/com/osman/Config.java
+++ src/main/java/com/osman/Config.java
@@ -6,11 +6,12 @@ import java.io.File;
  * Central application configuration that exposes default file-system locations
  * for fonts and shipping label PDFs.
  */
-public final class
-Config {
+public final class Config {
     private Config() {}
 
-
     public static final String DEFAULT_FONT_DIR =
-            System.getProperty("osman.fontDir", File.separator + "Users" + File.separator + "murattuncel" + File.separator + "Desktop" + File.separator + "Fonts");
+            System.getProperty(
+                    "fontDir",
+                    System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "Fonts"
+            );
 }
